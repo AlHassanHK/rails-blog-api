c910eedc89f79a8a4d4452a5076fe192e8d10368
AlHassanHK <alhassan.elkady@gmail.com>
2024-09-25 22:15:02 +0300

    feat: add default tags and one default user seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Tag.destroy_all
+
+tags = %w[
+  Technology
+  Science
+  Health
+  Travel
+  Food
+  Lifestyle
+  Education
+  Finance
+  Sports
+  Entertainment
+  Fashion
+  Music
+  Art
+  History
+  Nature
+]
+
+tags.each do |tag|
+  Tag.create(name: tag)
+end
+
+puts "Created #{Tag.count} tags."
+
+
+
+
+
+# Create default user
+default_user = User.create(
+  name: "Default User",
+  email: "default_user@example.com",
+  password: "secret",
+  password_confirmation: "secret"
+)
+
+puts "Created default user: #{default_user.email}"
